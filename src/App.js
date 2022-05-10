ed841d1edc6e962bce18914e017e229ae1538ebb
= <brennocesar@live.com>
2022-05-10 12:16:13 -0400

    Demo

---
+++ src/App.js
@@ -0,0 +1,10 @@
+export const App = () => {
+	return (
+		<div className='flex items-center justify-center'>
+			<i class='fas fa-user'></i>
+			<p>user</p>
+		</div>
+	);
+};
+
+export default App;
